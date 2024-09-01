1fc3eb9e34e81b31f3c0ef06feb0f8b497637ae7
Pedro Pinas <135555435+Pin4s@users.noreply.github.com>
2024-09-01 14:46:40 -0300

    Add files via upload

---
+++ FocusTimer/src/FocusTimer/sounds.js
@@ -0,0 +1,6 @@
+export const buttonPressAudio = new Audio('./assets/button-press.wav')
+
+export const kitchenTimer = new Audio('./assets/button-press.wav')
+
+export const bgAudio = new Audio('./assets/button-press.wav')
+
